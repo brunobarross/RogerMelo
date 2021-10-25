be031db441265f48a5a809198cd165bbe5590c7e
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-10-25 20:55:43 -0300

    Add files via upload

---
+++ Roger Mello/Exercicio Semana JS/app-aula.js
@@ -0,0 +1,17 @@
+//uma array de numero, quero obter a soma de todos os eles = reduce
+
+// essa funcao empre precisa retornar um valor, cada vez que esse valor é retornado o parametro acumulador receber o valor;
+
+// const numbers = [1,2,3]
+
+// const retornaSoma = numbers.reduce((acumulador, numero)=>{
+//     return acumulador + numero;
+// }, 0)
+
+
+// console.log(retornaSoma)
+
+
+
+
+
---
+++ Roger Mello/Exercicio Semana JS/app.js
@@ -0,0 +1,137 @@
+/* 01 gere um novo array com apenas os numeros impares doa rray abaixo e exiba o novo array no console.*/
+
+
+const randomNumbers = [10,30,15,25,50,40,5];
+
+const numerosImpares = randomNumbers.filter((numeroAleatorio)=>{
+    //se resto da divisão for 1 é impar
+    return numeroAleatorio % 2 ===1;
+})
+
+console.log(numerosImpares)
+
+
+
+/* 02- exiba no console quantos numeros abaixos de 501 o array abaixo possui */
+
+const crazyNumbers = [937,5,395,402,501,333,502,781,3,691];
+
+
+const menoresQue501 = crazyNumbers.reduce((accumulator, numero)=>{
+    return numero < 501 ? accumulator++ : accumulator;
+    // // if(numero< 501){
+    // //     accumulator += 1;
+    // // }
+
+    // return accumulator;
+}, 0)
+
+
+console.log(menoresQue501);
+
+
+
+/* 03 gere um novo array com cada um dos números abaixo elevados ao quadrado e exiba o novo array no console */
+
+const numeros = [5,7,3]
+
+const numerosElevados = numeros.map((numero)=>{
+    return numero ** 2;
+})
+
+console.log(numerosElevados)
+
+
+
+/*
+  04
+  - Utilizando o array abaixo, gere um novo array com apenas os filmes 
+    lançados antes do ano 2000;
+  - Exiba o novo array no console.
+*/
+
+const tarantinoMovies = [
+    { name: 'Bastardos inglórios', release: 2009 },
+    { name: 'Pulp Fiction', release: 1994 },
+    { name: 'Kill Bill: Volume 2', release: 2004 },
+    { name: 'Quatro Quartos', release: 1995 },
+    { name: 'Sin City', release: 2005 },
+    { name: 'Era uma Vez em... Hollywood', release: 2019 },
+    { name: 'Django Livre', release: 2012 },
+    { name: 'Cães de Aluguel', release: 1992 },
+    { name: 'À Prova de Morte', release: 2007 },
+    { name: 'Kill Bill: Volume 1', release: 2003 }
+  ]
+
+
+  const filmesAno2000 = tarantinoMovies.filter((tarantinoMovie)=>{
+      return tarantinoMovie.release < 2000;
+
+  })
+
+  console.log(filmesAno2000)
+
+
+
+  /*
+  05
+  - Gere um novo array que contém apenas os nomes das séries abaixo;
+  - Exiba o novo array no console.    
+*/
+
+const tvShows = [
+    { name: 'Breaking Bad', releaseYear: 2008 },
+    { name: 'Mr. Robot', releaseYear: 2015 },
+    { name: 'True Detective', releaseYear: 2014 },
+    { name: 'Hannibal', releaseYear: 2013 },
+    { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
+    { name: 'House M.D.', releaseYear: 2004 },
+    { name: 'Watchmen', releaseYear: 2019 }
+  ]
+  
+
+  const nomeShows = tvShows.map((tvShow)=>{
+      return tvShow.name;
+      // debugger insere breakpoint na execução do código (ver no console)
+     
+  })
+
+console.log(nomeShows)
+
+
+
+
+/*
+  06
+  - Exiba no console uma lista dos nomes dos jogos do array abaixo;
+  - A lista deve ter a formatação exemplificada abaixo do array, considerando 
+    inclusive o traço e o espaço antes de cada nome.
+  
+  Dica: para quebrar linha, você pode usar dentro da string o caractere 
+  especial \n.
+*/
+
+/* Se a partir do array nós queremos gerar uma string, é caso para uso do reduce pois permite gerar uma saída com todo tipo de dado*/
+
+
+const cart = [
+    { name: 'Dark Souls III', price: 95.03 },
+    { name: 'Shadow of the Tomb Raider', price: 101.19 },
+    { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
+    { name: 'Resident Evil 2', price: 119.90 },
+    { name: 'Death Stranding', price: 149.99 }
+  ]
+  
+  /*
+    - Nome 1
+    - Nome 2
+    - Nome 3
+  */
+
+
+    const listaProduto = cart.reduce((accumulator, produto)=>{
+        return `${accumulator} - ${produto.name},\n`;
+    }, '')
+
+
+    console.log(listaProduto);
